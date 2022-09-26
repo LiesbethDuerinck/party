1a29c2be985b0e70a44d55789bdd368ddd59fbeb
LiesbethDuerinck <r0903187@student.thomasmore.be>
2022-09-26 22:13:51 +0200

    les 1

--- src/main/java/be/thomasmore/party/controllers/HomeController.java
+++ src/main/java/be/thomasmore/party/controllers/HomeController.java
@@ -1,17 +1,28 @@
 package be.thomasmore.party.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class HomeController {
+     private final int mySpecialNumber = 35;
+    
     @GetMapping(value = {"/", "/home", "/home/"})
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("mySpecialNumber", mySpecialNumber);
         return "home";
     }
 
     @GetMapping("/about")
     public String about() {
         return "about";
+    }
+
+    @GetMapping("/venuedetails/{venuename}")
+    public String venuedetails(Model model, @PathVariable String venuename) {
+        model.addAttribute("venuename", venuename);
+        return "venuedetails";
     }
 }
