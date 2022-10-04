7e3075308ce30aee2eb6aba002309d6a1e1ffe3b
LiesbethDuerinck <r0903187@student.thomasmore.be>
2022-10-04 20:06:46 +0200

    week 3 met uitleg

--- src/main/java/be/thomasmore/party/PartyApplication.java
+++ src/main/java/be/thomasmore/party/PartyApplication.java
@@ -3,11 +3,11 @@ package be.thomasmore.party;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication              //zegt het Spring framework dat het Spring-magic moet gebr om de app op te starten
 public class PartyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PartyApplication.class, args);
-    }
+    }       //
 
 }
--- src/main/java/be/thomasmore/party/controllers/HomeController.java
+++ src/main/java/be/thomasmore/party/controllers/HomeController.java
@@ -1,7 +1,9 @@
+//Controller behandelt de user requests
+
 package be.thomasmore.party.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.ui.Model;                //om data van de Controller naar de View te sturen. MVC = Model, View, Controller
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import java.text.DateFormat;
@@ -9,31 +11,33 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
-@Controller
+@Controller         //deze annotatie vertelt Spring dat deze class een Controller is
 public class HomeController {
-     private final int mySpecialNumber = 35;
-     private final String[] venueNames ={"De Loods", "De Club", "De Hangar", "Cuba Libre"};
+     private final int mySpecialNumber = 35;        //final, want dit is een constante die nooit meer wordt veranderd. Nooit variabelen bijhouden in Controller!
+     private final String[] venueNames ={"De Loods", "De Club", "De Hangar", "Cuba Libre", "Zapoi", "Kuub"};        //altijd private + final
 
     
-    @GetMapping(value = {"/", "/home", "/home/"})
+    @GetMapping(value = {"/", "/home", "/home/"}) //deze annotatie vertelt Spring om deze methode aan te roepen als de Application Server de request "/" binnenkrijgt
     public String home(Model model) {
         model.addAttribute("mySpecialNumber", mySpecialNumber);
-        return "home";
+        return "home";                              //de view "home.html" zal gerendered worden
     }
 
-    @GetMapping("/about")
+    @GetMapping("/about")                               //een methode in de controller die requests behandelt. Spring herkent ze door @GetMapping = Request Handler.
+                                                            // Heeft steeds als return de naam van de View. View = wat de gebruiker zal zien
     public String about() {
         return "about";
     }
 
     @GetMapping(value = {"/venuedetails/{venueName}", "/venuedetails"})
     public String venuedetails(Model model,
-                               @PathVariable(required = false) String venueName){
-        model.addAttribute("venueNames", venueNames);
+                               @PathVariable(required = false) String venueName){       //via een PathVariable kan je data doorgeven van een page naar een andere
+                                                                                        //venueName = wat tussen accolades staat in eerste regel
+        model.addAttribute("venueNames", venueNames);           //volledige array wordt in het model gestoken
         return "venuedetails";
     }
 
-    @GetMapping("/pay")
+    @GetMapping("/pay")                                                     //elke url mapping moet uniek zijn binnen de springapplicatie
     public String pay(Model model){
         DateFormat format = new SimpleDateFormat("dd/MM/yyyy");
         Date today = new Date();
